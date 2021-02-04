f1ebb13a0f2f8229ba744a6690d2c529761cdcc1
Roman <pomankrentovsky@gmail.com>
2021-02-04 17:38:14 +0200

    beta version

--- app/js/slider.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-})();
